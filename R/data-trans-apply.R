3528eb36d3263502f3ddd4130fa0b72811faf37c
Wolfgang Peter <w.peter@statistik-peter.at>
2026-03-30 09:07:00 +0200

    Neue Funktion Index()

--- R/data-trans-apply.R
+++ R/data-trans-apply.R
@@ -124,3 +124,100 @@ dapply2 <- function (data,
 }
 
 
+#' Skalen zu einem Index zusammenfassen
+#'
+#' Berechnet einen zusammengesetzten Index aus mehreren Variablen
+#' (z. B. Items einer Skala). Optional können Variablen rekodiert,
+#' fehlende Werte behandelt und das Ergebnis als Prozentwert skaliert werden.
+#'
+#' @param ... Numerische Vektoren oder Faktoren, die zu einem Index
+#'   zusammengefasst werden sollen.
+#' @param na.rm Logisch. Sollen fehlende Werte bei der Berechnung
+#'   ignoriert werden? Standard ist TRUE.
+#' @param as_percent Logisch. Soll der Index auf den Bereich 0–1
+#'   skaliert werden? Standard ist TRUE.
+#' @param fun Aggregationsfunktion zur Bildung des Index
+#'   (z. B. mean, median oder sum). Standard ist mean mit as_percent.
+#' @param digits Anzahl der Nachkommastellen für das Ergebnis.
+#'   Standard ist 2.
+#' @param re_code Numerischer Index der Variablen, die rekodiert
+#'   werden sollen (z. B. für invertierte Items). Standard ist NULL.
+#' @param min_level Minimaler Skalenwert. Wird automatisch bestimmt,
+#'   wenn nicht angegeben.
+#' @param max_level Maximaler Skalenwert. Wird automatisch bestimmt,
+#'   wenn nicht angegeben.
+#'
+#' @returns Ein numerischer Vektor mit dem berechneten Index.
+#'
+#' @details
+#' Faktoren werden automatisch in numerische Werte umgewandelt.
+#' Wenn `re_code` gesetzt ist, werden die entsprechenden Variablen
+#' invertiert (Spiegelung an min/max).
+#'
+#' @export
+#'
+#' @examples
+#' # Beispiel: Bildung eines Index aus mehreren Items
+#' Anamnesebogen <- Anamnesebogen |>
+#'   mutate(
+#'     wechsel = Index(
+#'       hitzewallungen.schwitzen,
+#'       depressive.verstimmungen,
+#'       reizbarkeit,
+#'       angstlichkeit,
+#'       herzbeschwerden,
+#'       vergesslichkeit,
+#'       haarausfall
+#'     )
+#'   )
+#'
+#' Anamnesebogen$wechsel
+Index <- function(...,
+                  na.rm = TRUE,
+                  
+                  as_percent = TRUE,
+                  fun = mean,
+                  digits = 2,
+                  re_code = NULL,
+                  min_level = NA,
+                  max_level = NA) {
+  dots <- list(...)
+  if (is.na(min_level) & is.na(max_level) & is.factor(dots[[1]])) {
+    min_level <- 1
+    max_level <-  nlevels(dots[[1]])
+  }
+  dots <- sapply(dots, as.numeric)
+  
+  if (is.numeric(re_code))
+    dots <- Umcodieren(dots, re_code, min_level, max_level)
+  
+  dots <-  apply(dots, 1, fun, na.rm = na.rm)
+  if (as_percent) {
+    dots <-  (dots - min_level) / (max_level - min_level)
+    
+  }
+  
+  round(dots, digits)
+}
+#' @noRd
+Umcodieren <- function(x,
+                       re_code,
+                       min_level = NA,
+                       max_level = NA) {
+  if (is.na(max_level))
+    max_level <- max(x, na.rm = TRUE)
+  if (is.na(min_level))
+    min_level <- min(x, na.rm = TRUE)
+  mytempdata <- x[, re_code]
+  
+  if (is.numeric(mytempdata))
+    x[, re_code] <- max_level + min_level - mytempdata
+  else
+    x[, re_code] <-
+    apply(mytempdata, 2, function(item)
+      max_level + min_level - item)
+  return(x)
+}
+
+
+
